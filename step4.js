92591ffbd6e6bac0999c789b422e34fd0b79e38b
kevinmacarthur <kmacarthur16@gmail.com>
2018-10-01 17:10:58 +0000

    completed step 4 adding callback feature

---
+++ step4.js
@@ -0,0 +1,33 @@
+var https = require('https')
+
+
+function getHTML (options, callback) {
+
+  var data = [];
+  https.get(options, function(response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(chunk) {
+      data.push(chunk);
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete')
+      var final = data.join('')
+      callback(final)
+    });
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML)
